c078ff62da8373ea4b752f1ffe79a727eb6b8bec
Anders Sveen <anders@f12.no>
2024-06-18 06:16:51 +0200

    Fixed exports

--- formjson.js
+++ formjson.js
@@ -67,8 +67,31 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
-module.exports = {
-    formDataToList: formDataToList,
-    convertToObjectHierarchy: convertToObjectHierarchy,
-    form_to_object: form_to_object,
-};
+
+// Export modules if in Node.js
+// Thanks to https://stackoverflow.com/questions/4224606/how-to-check-whether-a-script-is-running-under-node-js
+(function () {
+
+    // Establish the root object, `window` in the browser, or `global` on the server.
+    var root = this;
+
+    // Create a reference to this
+    var _ = new Object();
+
+    var isNode = false;
+
+    // Export the Underscore object for **CommonJS**, with backwards-compatibility
+    // for the old `require()` API. If we're not in CommonJS, add `_` to the
+    // global object.
+    if (typeof module !== 'undefined' && module.exports) {
+        module.exports = {
+            formDataToList: formDataToList,
+            convertToObjectHierarchy: convertToObjectHierarchy,
+            form_to_object: form_to_object,
+        };
+        root._ = _;
+        isNode = true;
+    } else {
+        root._ = _;
+    }
+})();
